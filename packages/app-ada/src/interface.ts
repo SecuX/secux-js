fffda4d978bf8ee4f094be7445d38cabe3699633
CYCken <46667542+CYCken@users.noreply.github.com>
2026-03-25 14:02:56 +0800

    feat: Cardano * Introduce TextEncoder/TextDecoder polyfills * enhance  address derivation with stake index * transaction signing with token

--- packages/app-ada/src/interface.ts
+++ packages/app-ada/src/interface.ts
@@ -2,6 +2,7 @@
 Copyright 2022 SecuX Technology Inc
 Copyright Chen Wei-En
 Copyright Wu Tsung-Yu
+Copyright Chang Chia-Yu
 
 Licensed under the Apache License, Version 2.0 (the License);
 you may not use this file except in compliance with the License.
@@ -41,6 +42,7 @@ export type PublickeyOption = {
 const txId = ow.string.matches(/^[0-9A-Fa-f]{64}$/);
 const index = ow.number.uint8;
 const amount = ow.any(ow.number.uint32.positive, owTool.numberString);
+const roleIndex = ow.optional.number.uint8;
 const addressIndex = ow.optional.number.uint8;
 const stakeIndex = ow.optional.number.uint8;
 export const ow_PublickeyOption = ow.object.exactShape({
@@ -93,7 +95,8 @@ export type AddressOption = {
     addressIndex?: number,
     stakeIndex?: number,
     pointer?: PointerOption,
-    network?: typeof NetworkInfo.mainnet
+    network?: typeof NetworkInfo.mainnet,
+    needChange?: boolean
 };
 
 export const ow_AddressOption = ow.object.partialShape({
@@ -108,7 +111,13 @@ export type TxInput = {
     amount: number | string,
     xpublickey?: string | Buffer,
     addressIndex?: number,
-    stakeIndex?: number
+    roleIndex?: number,
+    stakeIndex?: number,
+    tokens?: Array<{
+      policyId: string;
+      assetName: string;
+      quantity: number | string;
+    }>;
 };
 
 export const ow_TxInput = ow.object.exactShape({
@@ -117,8 +126,10 @@ export const ow_TxInput = ow.object.exactShape({
     index,
     amount,
     xpublickey: ow_xpublickey,
+    roleIndex,
     addressIndex,
-    stakeIndex
+    stakeIndex,
+    tokens: ow.any(ow.null, ow.array, ow.undefined),
 });
 
 export type TxOutput = {
@@ -203,6 +214,7 @@ export const ow_UnStakeOption = ow.object.exactShape({
 export interface WithdrawOption extends BaseOption {
     stakeIndex?: number;
     network?: typeof NetworkInfo.mainnet;
+    needVote?: boolean;
 }
 
 export const ow_WithdrawOption = ow.object.exactShape({
